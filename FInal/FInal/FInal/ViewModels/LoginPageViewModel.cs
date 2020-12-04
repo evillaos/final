793fe44b01864ad35be9d5d6de24c677e40d5aa1
evillaos <nemesis.edi@gmail.com>
2020-12-03 21:17:39 -0500

    Add login page

---
+++ FInal/FInal/FInal/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FInal.ViewModels
+{
+    public class LoginPageViewModel : ViewModelBase
+    {
+        private readonly INavigationService _navigationService;
+        private bool _isEnabled;
+        private DelegateCommand _loginFacebookCommand;
+        public LoginPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            _navigationService = navigationService;
+            Title = "Login";
+            IsEnabled = true;
+        }
+
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set => SetProperty(ref _isEnabled, value);
+        }
+        public DelegateCommand LoginFacebookCommand => _loginFacebookCommand ?? (_loginFacebookCommand = new DelegateCommand(LoginFacebookAsync));
+
+        private async void LoginFacebookAsync()
+        {
+            await App.Current.MainPage.DisplayAlert("Error", "Funciona", "Aceptar");
+        }
+
+    }
+}
---
+++ FInal/FInal/FInal/Views/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FInal.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
